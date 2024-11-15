90f9bd3baa0be7030b70239ae62e83654c2983c1
Bruno Enrique <bruno.verdo@becompliance.com>
2024-11-15 16:13:12 -0300

    chore(shutdown): add graceful shutdown to index file

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,13 @@ import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
-const FIVE_MINUTES_IN_MILISECONDS = 5 * 60 * 1000;
+const TEN_SECONDS_IN_MILLISECONDS = 10 * 1000;
+const FIVE_MINUTES_IN_MILLISECONDS = 5 * 60 * 1000;
 const MAX_REQUEST_PER_IP = 100;
 
 app.use(
   rateLimit({
-    windowMs: FIVE_MINUTES_IN_MILISECONDS,
+    windowMs: FIVE_MINUTES_IN_MILLISECONDS,
     limit: MAX_REQUEST_PER_IP
   })
 );
@@ -26,6 +27,33 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.listen(env.PORT, () => {
-  logger.debug(`Server is listening in port: ${env.PORT}`);
+const server = app.listen(env.PORT, async () => {
+  logger.info(`Server is listening in port: ${env.PORT}`);
 });
+
+const gracefulShutdown = (signal: string) => {
+  return async () => {
+    const shutdownTimeout = setTimeout(() => {
+      logger.info('Forcing shutdown due to timeout');
+      process.exit(1);
+    }, TEN_SECONDS_IN_MILLISECONDS);
+
+    logger.info(`Received signal to terminate: ${signal}`);
+
+    server.close(async (err) => {
+      clearTimeout(shutdownTimeout);
+      if (err) {
+        logger.error('Error while closing server:', err);
+        process.exit(1);
+      }
+
+      logger.info('HTTP server closed.');
+
+      process.exit(0);
+    });
+  };
+};
+
+process.on('SIGTERM', gracefulShutdown('SIGTERM'));
+
+process.on('SIGINT', gracefulShutdown('SIGINT'));
